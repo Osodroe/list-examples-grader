6ebd06b65d57ea8f24b2b03f7e3c2f8750d972a3
Osodroe <82431412+Osodroe@users.noreply.github.com>
2022-11-10 13:49:19 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,7 +1,21 @@
 # Create your grading script here
 
+CPATH = ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 set -e
 
 rm -rf student-submission
 git clone $1 student-submission
 
+cp GradeServer.java grade/
+
+cd grade 
+
+javac -cp $CPATH *.java
+
+if [[$? -eq 0]]
+then
+    echo "Compile Succeeded"
+else
+    echo "Your program didn't compile"
+    exit 1
+fi
