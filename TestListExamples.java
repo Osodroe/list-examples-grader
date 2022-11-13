11c9bdda370f4859f0c0e2509ab622594940dd50
Osodroe <osodroe@gmail.com>
2022-11-12 20:54:11 -0800

    All but real tests

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,45 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
+
+class StringCheck implements StringChecker {
+  public boolean checkString(String s) {
+    if(s.length() > 5) {return true;}
+    else {return false;}
+  }
+}
 
 public class TestListExamples {
-  // Write your grading tests here!
+  @Test
+    public void filter4() {
+        List<String> input1 = new ArrayList<>();
+        List<String> expected1 = new ArrayList<>();
+        StringChecker sc = new StringCheck();
+        input1.add("sandwich");
+        input1.add("crab");
+        input1.add("sand");
+        input1.add("towers");
+        expected1.add("sandwich");
+        expected1.add("towers");
+        assertEquals(expected1, ListExamples.filter(input1, sc));
+    }
+
+    @Test
+    public void merge5() {
+        List<String> input1 = new ArrayList<>();
+        List<String> input2 = new ArrayList<>();
+        List<String> expected1 = new ArrayList<>();
+        input2.add("crab");
+        input2.add("power");
+        input2.add("sand");
+        input1.add("sandwich");
+        input2.add("towers");
+        expected1.add("crab");
+        expected1.add("power");
+        expected1.add("sand");
+        expected1.add("sandwich");
+        expected1.add("towers");
+        assertEquals(expected1, ListExamples.merge(input1, input2));
+    }
 }
--- grade.sh
+++ grade.sh
@@ -1,21 +1,44 @@
 # Create your grading script here
 
-CPATH = ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
-set -e
+CPATH=".;./lib/hamcrest-core-1.3.jar;./lib/junit-4.13.2.jar"
 
 rm -rf student-submission
-git clone $1 student-submission
+git clone -q $1 student-submission/
 
-cp GradeServer.java grade/
+FPATH=$(find student-submission/* -type f)
 
-cd grade 
+if [ $? -eq 0 ]
+then   
+    echo "Clone succeeded"
+else
+    echo "Repository not found"
+    exit 1
+fi
+
+FFILE=$(basename $FPATH)
+
+if [[ $FFILE == "ListExamples.java" ]]
+then
+    echo "File found"
+else   
+    echo "$FFILE is incorrect file name"
+    exit 1
+fi
+
+FDIR=$(dirname $FPATH)
+
+cp TestListExamples.java $FDIR
+cp -r lib $FDIR
+cd $FDIR
 
 javac -cp $CPATH *.java
 
-if [[$? -eq 0]]
+if [ $? -eq 0 ]
 then
-    echo "Compile Succeeded"
+    echo "Compile succeeded"
 else
     echo "Your program didn't compile"
     exit 1
 fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
